b4eab86055bd5bee78bb1c0d62154656ff7d147b
tstelter <tobias.stelter@outlook.com>
2022-03-26 20:39:57 +0100

    added text-strings

--- model/WavePatternFinder/WavePattern.py
+++ model/WavePatternFinder/WavePattern.py
@@ -1,5 +1,21 @@
 class WavePattern(object):
 
     def __init__(self, wave_list, name):
-        self.name = name
-        self.wave_list = wave_list
+        self._name = name
+        self._wave_list = wave_list
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def wave_list(self, name):
+        self._name = name
+
+    @property
+    def wave_list(self):
+        return self._wave_list
+
+    @name.setter
+    def name(self, wave_list):
+        self._wave_list = wave_list
